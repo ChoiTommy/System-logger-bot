6df3e31cffcaef1a0787aa8101ace86b96d320de
Kaito <tommychoiht@gmail.com>
2021-04-29 17:11:26 +0800

    Added reaction inline buttons

--- main.py
+++ main.py
@@ -31,6 +31,20 @@ keyboard = [[InlineKeyboardButton("✔", callback_data='1'), InlineKeyboardButto
 approved_keyboard = [[InlineKeyboardButton("✅Approved and posted", callback_data='-1')]]
 rejected_keyboard = [[InlineKeyboardButton("❌Rejected", callback_data='-1')]]
 about_keyboard = [[InlineKeyboardButton("Github", url='https://github.com/ChoiTommy/System-logger-bot')]]
+emoji_list = ["👍", "😲", "🤨", "👎"]
+reaction_callback_data_list = ['1000', '1001', '1002', '1003']
+reactions_keyboard = [[
+    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data=reaction_callback_data_list[0]),
+    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data=reaction_callback_data_list[1]),
+    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data=reaction_callback_data_list[2]),
+    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data=reaction_callback_data_list[3]),
+    ]]
+reactions_keyboard_for_display = [[
+    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data='-1'),
+    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data='-1'),
+    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data='-1'),
+    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data='-1'),
+    ]]
 
 def authentication(update: Update) -> bool:
     return update.effective_user.username == MY_USERNAME
@@ -47,6 +61,75 @@ def ask_for_text(update: Update, context: CallbackContext) -> int:
         update.message.reply_text('⛔️You are not allowed to use this command.')
         return ConversationHandler.END
 
+def ask_for_photo_with_text(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    context.user_data['TEXT'] = text
+    update.message.reply_text(
+        '📸Now send me an image if you wish to. Otherwise click the button below to skip this step. (Currently only the first image will be captured.)',
+        reply_markup = ReplyKeyboardMarkup([['Post without image']], resize_keyboard = True)
+    )
+    return 1
+
+def ask_for_photo_without_text(update: Update, context: CallbackContext) -> int:
+    context.user_data['TEXT'] = ''
+    update.message.reply_text(
+        '📸Now send me an image that you wanna post. Currently only the first image will be captured.',
+        reply_markup = ReplyKeyboardRemove()
+    )
+    return 2
+
+def include_reactions(update: Update, context: CallbackContext) -> int:
+    # retrive photo sent by user
+    photo = update.message.photo
+    context.user_data['WITH_PHOTO'] = False if not photo else True
+    context.user_data['PHOTO'] = photo[0] if photo else None
+    update.message.reply_text(
+        '♥Do you want to append reaction buttons to this post? Select the buttons below.',
+        reply_markup = ReplyKeyboardMarkup([['Yes', 'No']], resize_keyboard = True)
+    )
+    return 3
+
+def confirmation(update: Update, context: CallbackContext) -> int:
+    context.user_data['WITH_REACTIONS'] = True if update.message.text == 'Yes' else False
+    if context.user_data['WITH_PHOTO']:
+        update.message.reply_photo(
+            context.user_data['PHOTO'],
+            caption=context.user_data['TEXT'],
+            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None
+        )
+    else:
+        update.message.reply_text(
+            context.user_data['TEXT'],
+            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None
+        )
+    update.message.reply_text(
+        '❓The above message is going to be posted in the channel. Are you sure you want to do so?',
+        reply_markup = ReplyKeyboardMarkup([['Yes', 'No']], resize_keyboard = True)
+    )
+    return 4
+
+def send(update: Update, context: CallbackContext) -> int:
+    choice = update.message.text
+    if choice == 'Yes':
+        if context.user_data['WITH_PHOTO']:
+            context.bot.send_photo(
+                MY_CHANNEL_ID,
+                context.user_data['PHOTO'],
+                caption=context.user_data['TEXT'],
+                reply_markup=InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None
+            )
+        else:
+            context.bot.send_message(
+                MY_CHANNEL_ID,
+                context.user_data['TEXT'],
+                reply_markup=InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None
+            )
+        update.message.reply_text('✅Sent successfully.', reply_markup=ReplyKeyboardRemove(), disable_notification=True)
+    else:
+        update.message.reply_text('🗑Message discarded. Type /new to post again.', reply_markup=ReplyKeyboardRemove())
+    context.user_data.clear()
+    return ConversationHandler.END
+
 # for post submission
 def ask_for_text_submission(update: Update, context: CallbackContext) -> int:
     if authentication(update):
@@ -60,39 +143,6 @@ def ask_for_text_submission(update: Update, context: CallbackContext) -> int:
         )
         return 0
 
-def ask_for_photo_without_text(update: Update, context: CallbackContext) -> int:
-    context.user_data['TEXT'] = ''
-    update.message.reply_text(
-        '📸Now send me an image that you wanna post. Currently only the first image will be captured.',
-        reply_markup = ReplyKeyboardRemove()
-    )
-    return 2
-
-def ask_for_photo_with_text(update: Update, context: CallbackContext) -> int:
-    text = update.message.text
-    context.user_data['TEXT'] = text
-    update.message.reply_text(
-        '📸Now send me an image if you wish to. Otherwise click the button below to skip this step. (Currently only the first image will be captured.)',
-        reply_markup = ReplyKeyboardMarkup([['Post without image']], resize_keyboard = True)
-    )
-    return 1
-
-def confirmation(update: Update, context: CallbackContext) -> int:
-    photo = update.message.photo
-    context.user_data['WITH_PHOTO'] = False if not photo else True
-    context.user_data['PHOTO'] = photo[0] if photo else None
-
-    if context.user_data['WITH_PHOTO']:
-        update.message.reply_photo(context.user_data['PHOTO'], caption=context.user_data['TEXT'])
-    else:
-        update.message.reply_text(context.user_data['TEXT'])
-
-    update.message.reply_text(
-        '❓The above message is going to be posted in the channel. Are you sure you want to do so?',
-        reply_markup = ReplyKeyboardMarkup([['Yes', 'No']], resize_keyboard = True)
-    )
-    return 3
-
 def submission_confirmation(update: Update, context: CallbackContext) -> int:
     photo = update.message.photo
     context.user_data['WITH_PHOTO'] = False if not photo else True
@@ -100,28 +150,21 @@ def submission_confirmation(update: Update, context: CallbackContext) -> int:
     context.user_data['TEXT'] = f"{context.user_data['TEXT']}\n - {update.effective_user.first_name} {'' if update.effective_user.last_name == None else update.effective_user.last_name }"
 
     if context.user_data['WITH_PHOTO']:
-        update.message.reply_photo(context.user_data['PHOTO'], caption=context.user_data['TEXT'])
+        update.message.reply_photo(
+            context.user_data['PHOTO'],
+            caption=context.user_data['TEXT'],
+            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display)
+        )
     else:
-        update.message.reply_text(context.user_data['TEXT'])
-
+        update.message.reply_text(
+            context.user_data['TEXT'],
+            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display)
+        )
     update.message.reply_text(
         '❓The above message is going to be sent to the channel owner for approval. Are you sure you want to do so?',
         reply_markup = ReplyKeyboardMarkup([['Yes', 'No']], resize_keyboard = True)
     )
     return 3
-
-def send(update: Update, context: CallbackContext) -> int:
-    choice = update.message.text
-    if choice == 'Yes':
-        if context.user_data['WITH_PHOTO']:
-            context.bot.send_photo(MY_CHANNEL_ID, context.user_data['PHOTO'], caption=context.user_data['TEXT'])
-        else:
-            context.bot.send_message(MY_CHANNEL_ID, context.user_data['TEXT'])
-        update.message.reply_text('✅Sent successfully.', reply_markup=ReplyKeyboardRemove(), disable_notification=True)
-    else:
-        update.message.reply_text('🗑Message discarded. Type /new to post again.', reply_markup=ReplyKeyboardRemove())
-    context.user_data.clear()
-    return ConversationHandler.END
 
 def send_to_owner(update: Update, context: CallbackContext) -> int:
     choice = update.message.text
@@ -130,7 +173,10 @@ def send_to_owner(update: Update, context: CallbackContext) -> int:
             context.bot.send_photo(MY_ID, context.user_data['PHOTO'], caption=context.user_data['TEXT'], reply_markup=InlineKeyboardMarkup(keyboard))
         else:
             context.bot.send_message(MY_ID, context.user_data['TEXT'], reply_markup=InlineKeyboardMarkup(keyboard))
-        update.message.reply_text('✅Sent successfully. If your post is approved, it will appear in the channel. Stay tuned!', reply_markup=ReplyKeyboardRemove(), disable_notification=True)
+        update.message.reply_text('✅Sent successfully. If your post is approved, it will appear in the channel. Stay tuned!',
+            reply_markup=ReplyKeyboardRemove(),
+            disable_notification=True
+        )
     else:
         update.message.reply_text('🗑Message discarded. Type /submit to start again.', reply_markup=ReplyKeyboardRemove())
     context.user_data.clear()
@@ -141,7 +187,7 @@ def cancel(update: Update, context: CallbackContext) -> int:
     context.user_data.clear()
     return ConversationHandler.END
 
-def approval(update: Update, _: CallbackContext) -> None:
+def inline_buttons(update: Update, _: CallbackContext) -> None:
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
@@ -149,9 +195,14 @@ def approval(update: Update, _: CallbackContext) -> None:
     query.answer()
     if query.data == '1':
         query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(approved_keyboard))
-        query.copy_message(MY_CHANNEL_ID)
+        query.copy_message(MY_CHANNEL_ID, reply_markup=InlineKeyboardMarkup(reactions_keyboard))
     elif query.data == '0':
         query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(rejected_keyboard))
+    elif query.data in reaction_callback_data_list:
+        keyboard = query.message.reply_markup.inline_keyboard
+        number = int(keyboard[0][int(query.data)-1000].text[2::]) + 1
+        keyboard[0][int(query.data)-1000].text = f'{emoji_list[int(query.data)-1000]} {number}'
+        query.edit_message_reply_markup(InlineKeyboardMarkup(keyboard))
 
 def about(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
@@ -173,9 +224,10 @@ def main() -> None:
         entry_points = [CommandHandler('new', ask_for_text)],
         states = { # dict
             0: [MessageHandler(Filters.regex('^Post without text$'), ask_for_photo_without_text), MessageHandler(Filters.text & ~Filters.command, ask_for_photo_with_text)],
-            1: [MessageHandler(Filters.photo | Filters.regex('^Post without image$'), confirmation)],
-            2: [MessageHandler(Filters.photo, confirmation)],
-            3: [MessageHandler(Filters.regex('^Yes$') | Filters.regex('^No$'), send)]
+            1: [MessageHandler(Filters.photo | Filters.regex('^Post without image$'), include_reactions)],
+            2: [MessageHandler(Filters.photo, include_reactions)],
+            3: [MessageHandler(Filters.regex('^Yes$') | Filters.regex('^No$'), confirmation)],
+            4: [MessageHandler(Filters.regex('^Yes$') | Filters.regex('^No$'), send)]
         },
         fallbacks = [CommandHandler('cancel', cancel)],
         conversation_timeout = 300 # 5 mins
@@ -196,7 +248,7 @@ def main() -> None:
     )
 
     dispatcher.add_handler(submit_post_handler)
-    dispatcher.add_handler(CallbackQueryHandler(approval))
+    dispatcher.add_handler(CallbackQueryHandler(inline_buttons))
 
     # add handler for /about
     dispatcher.add_handler(CommandHandler('about', about))
