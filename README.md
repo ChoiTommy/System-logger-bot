33c296c6a8e6c15255a8df6e490ad06bbde901df
Kaito <tommychoiht@gmail.com>
2021-04-30 13:26:55 +0800

    New README.md and now supports text formatting

---
+++ README.md
@@ -0,0 +1,38 @@
+# System Logger Bot
+A custom telegram channel bot built for my own channel. It supports post submissions for both channel owner and channel subscribers. Optional reaction buttons can be appended to each channel post for engaging interaction with subscribers. 
+
+Features:
+
+- Post submission for channel owner (with or without image, text, reaction buttons)
+- Post submission for outsiders (with or without image, text)
+- Text supports formatting features
+- Approval for post submissions from outsiders
+- Reactions number counts
+
+## Quick Start
+
+---
+
+### Setup
+
+It is only compatible with Python versions 3.6+. Make sure a proper version of Python is installed.
+
+The proceed to install [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot) with:
+
+```bash
+$ pip install python-telegram-bot
+```
+
+Install [python-dotenv](https://github.com/theskumar/python-dotenv) if your credentials are saved in a `.env` file and to be accessed during runtime:
+
+```bash
+$ pip install python-dotenv
+```
+
+### Run
+
+Key in your own bot token and respective IDs to a `.env` file or the corresponding places in the code. You may need to change a portion of the code to fit your situation. Finally, run the following command: 
+
+```bash
+$ python main.py
+```
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
  https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
  https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/inlinekeyboard.py
 '''
-# TODO drafting function, multiple photo attachments, text formatting
+# TODO drafting function, multiple photo attachments, feedback to approved or rejected posts
 
 import logging, os
 
@@ -31,19 +31,19 @@ keyboard = [[InlineKeyboardButton("✔", callback_data='1'), InlineKeyboardButto
 approved_keyboard = [[InlineKeyboardButton("✅Approved and posted", callback_data='-1')]]
 rejected_keyboard = [[InlineKeyboardButton("❌Rejected", callback_data='-1')]]
 about_keyboard = [[InlineKeyboardButton("Github", url='https://github.com/ChoiTommy/System-logger-bot')]]
-emoji_list = ["👍", "😲", "🤨", "👎"]
+emoji_list = ["👍", "😲", "🤨", "🤬"]
 reaction_callback_data_list = ['1000', '1001', '1002', '1003']
 reactions_keyboard = [[
-    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data=reaction_callback_data_list[0]),
-    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data=reaction_callback_data_list[1]),
-    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data=reaction_callback_data_list[2]),
-    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data=reaction_callback_data_list[3]),
+    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data = reaction_callback_data_list[0]),
+    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data = reaction_callback_data_list[1]),
+    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data = reaction_callback_data_list[2]),
+    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data = reaction_callback_data_list[3]),
     ]]
 reactions_keyboard_for_display = [[
-    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data='-1'),
-    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data='-1'),
-    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data='-1'),
-    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data='-1'),
+    InlineKeyboardButton(f'{emoji_list[0]} 0', callback_data = '-1'),
+    InlineKeyboardButton(f'{emoji_list[1]} 0', callback_data = '-1'),
+    InlineKeyboardButton(f'{emoji_list[2]} 0', callback_data = '-1'),
+    InlineKeyboardButton(f'{emoji_list[3]} 0', callback_data = '-1'),
     ]]
 
 def authentication(update: Update) -> bool:
@@ -62,8 +62,8 @@ def ask_for_text(update: Update, context: CallbackContext) -> int:
         return ConversationHandler.END
 
 def ask_for_photo_with_text(update: Update, context: CallbackContext) -> int:
-    text = update.message.text
-    context.user_data['TEXT'] = text
+    context.user_data['TEXT'] = update.message.text
+    context.user_data['ENTITIES'] = update.message.entities
     update.message.reply_text(
         '📸Now send me an image if you wish to. Otherwise click the button below to skip this step. (Currently only the first image will be captured.)',
         reply_markup = ReplyKeyboardMarkup([['Post without image']], resize_keyboard = True)
@@ -72,6 +72,7 @@ def ask_for_photo_with_text(update: Update, context: CallbackContext) -> int:
 
 def ask_for_photo_without_text(update: Update, context: CallbackContext) -> int:
     context.user_data['TEXT'] = ''
+    context.user_data['ENTITIES'] = None
     update.message.reply_text(
         '📸Now send me an image that you wanna post. Currently only the first image will be captured.',
         reply_markup = ReplyKeyboardRemove()
@@ -94,13 +95,15 @@ def confirmation(update: Update, context: CallbackContext) -> int:
     if context.user_data['WITH_PHOTO']:
         update.message.reply_photo(
             context.user_data['PHOTO'],
-            caption=context.user_data['TEXT'],
-            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None
+            caption = context.user_data['TEXT'],
+            reply_markup = InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None,
+            caption_entities = context.user_data['ENTITIES']
         )
     else:
         update.message.reply_text(
             context.user_data['TEXT'],
-            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None
+            reply_markup = InlineKeyboardMarkup(reactions_keyboard_for_display) if context.user_data['WITH_REACTIONS'] else None,
+            entities = context.user_data['ENTITIES']
         )
     update.message.reply_text(
         '❓The above message is going to be posted in the channel. Are you sure you want to do so?',
@@ -115,18 +118,22 @@ def send(update: Update, context: CallbackContext) -> int:
             context.bot.send_photo(
                 MY_CHANNEL_ID,
                 context.user_data['PHOTO'],
-                caption=context.user_data['TEXT'],
-                reply_markup=InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None
+                caption = context.user_data['TEXT'],
+                reply_markup = InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None,
+                caption_entities = context.user_data['ENTITIES'],
+                disable_notification = True
             )
         else:
             context.bot.send_message(
                 MY_CHANNEL_ID,
                 context.user_data['TEXT'],
-                reply_markup=InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None
+                reply_markup = InlineKeyboardMarkup(reactions_keyboard) if context.user_data['WITH_REACTIONS'] else None,
+                entities = context.user_data['ENTITIES'],
+                disable_notification = True
             )
-        update.message.reply_text('✅Sent successfully.', reply_markup=ReplyKeyboardRemove(), disable_notification=True)
+        update.message.reply_text('✅Sent successfully.', reply_markup = ReplyKeyboardRemove(), disable_notification = True)
     else:
-        update.message.reply_text('🗑Message discarded. Type /new to post again.', reply_markup=ReplyKeyboardRemove())
+        update.message.reply_text('🗑Message discarded. Type /new to post again.', reply_markup = ReplyKeyboardRemove())
     context.user_data.clear()
     return ConversationHandler.END
 
@@ -152,13 +159,15 @@ def submission_confirmation(update: Update, context: CallbackContext) -> int:
     if context.user_data['WITH_PHOTO']:
         update.message.reply_photo(
             context.user_data['PHOTO'],
-            caption=context.user_data['TEXT'],
-            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display)
+            caption = context.user_data['TEXT'],
+            reply_markup = InlineKeyboardMarkup(reactions_keyboard_for_display),
+            caption_entities = context.user_data['ENTITIES']
         )
     else:
         update.message.reply_text(
             context.user_data['TEXT'],
-            reply_markup=InlineKeyboardMarkup(reactions_keyboard_for_display)
+            reply_markup = InlineKeyboardMarkup(reactions_keyboard_for_display),
+            entities = context.user_data['ENTITIES']
         )
     update.message.reply_text(
         '❓The above message is going to be sent to the channel owner for approval. Are you sure you want to do so?',
@@ -170,20 +179,30 @@ def send_to_owner(update: Update, context: CallbackContext) -> int:
     choice = update.message.text
     if choice == 'Yes':
         if context.user_data['WITH_PHOTO']:
-            context.bot.send_photo(MY_ID, context.user_data['PHOTO'], caption=context.user_data['TEXT'], reply_markup=InlineKeyboardMarkup(keyboard))
+            context.bot.send_photo(
+                MY_ID,
+                context.user_data['PHOTO'],
+                caption = context.user_data['TEXT'],
+                reply_markup = InlineKeyboardMarkup(keyboard),
+                caption_entities = context.user_data['ENTITIES']
+            )
         else:
-            context.bot.send_message(MY_ID, context.user_data['TEXT'], reply_markup=InlineKeyboardMarkup(keyboard))
+            context.bot.send_message(
+                MY_ID,
+                context.user_data['TEXT'],
+                reply_markup = InlineKeyboardMarkup(keyboard),
+                entities = context.user_data['ENTITIES']
+            )
         update.message.reply_text('✅Sent successfully. If your post is approved, it will appear in the channel. Stay tuned!',
-            reply_markup=ReplyKeyboardRemove(),
-            disable_notification=True
+            reply_markup = ReplyKeyboardRemove()
         )
     else:
-        update.message.reply_text('🗑Message discarded. Type /submit to start again.', reply_markup=ReplyKeyboardRemove())
+        update.message.reply_text('🗑Message discarded. Type /submit to start again.', reply_markup = ReplyKeyboardRemove())
     context.user_data.clear()
     return ConversationHandler.END
 
 def cancel(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('ℹ️The process is cancelled. Send the command again to restart the program.', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text('ℹ️The process is cancelled. Send the command again to restart the program.', reply_markup = ReplyKeyboardRemove())
     context.user_data.clear()
     return ConversationHandler.END
 
@@ -191,13 +210,12 @@ def inline_buttons(update: Update, _: CallbackContext) -> None:
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
     if query.data == '1':
-        query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(approved_keyboard))
-        query.copy_message(MY_CHANNEL_ID, reply_markup=InlineKeyboardMarkup(reactions_keyboard))
+        query.edit_message_reply_markup(reply_markup = InlineKeyboardMarkup(approved_keyboard))
+        query.copy_message(MY_CHANNEL_ID, reply_markup = InlineKeyboardMarkup(reactions_keyboard), disable_notification = True)
     elif query.data == '0':
-        query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(rejected_keyboard))
+        query.edit_message_reply_markup(reply_markup = InlineKeyboardMarkup(rejected_keyboard))
     elif query.data in reaction_callback_data_list:
         keyboard = query.message.reply_markup.inline_keyboard
         number = int(keyboard[0][int(query.data)-1000].text[2::]) + 1
@@ -206,7 +224,7 @@ def inline_buttons(update: Update, _: CallbackContext) -> None:
 
 def about(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
-        '@system\_logger\_bot *Beta*\n\nDedicated for _System Logs_',
+        '@system\_logger\_bot *Beta*\n\nTailored for _System Logs_',
         parse_mode = ParseMode.MARKDOWN_V2,
         reply_markup = InlineKeyboardMarkup(about_keyboard)
     )
